9c7615bab6d4883cf98e55e95ab0ae925a707c22
Bart <vdheijden.bart@gmail.com>
2018-05-29 09:31:49 +0200

    function intersectpoint

--- Bouncing.py
+++ Bouncing.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
 from numpy import *
-from numpy.linalg import solve
+from numpy.linalg import solve,norm
 
 def intersect(a,b,p,q):
     x=[[b[0]-a[0],p[0]-q[0]],[b[1]-a[1],p[1]-q[1]]]
     y=[[p[0]-a[0]],[p[1]-a[1]]]
     z=solve(x,y)
-    return z
+    lam=z[0]
+    mu=z[1]
+    return lam,mu
 
+def intersectpoint(a,b,lam):
+    x=[b[0]-a[0],b[1]-a[1]]
+    y=[lam*x[0],lam*x[1]]    
+    s=[a[0]+y[0],a[1]+y[1]]
+    return s
+
+
+a=[1.,4]
+b=[2.,1]
+p=[3.,6]
+q=[1.,2]
+
+lam,mu=intersect(a,b,p,q)
+s=intersectpoint(a,b,lam)
