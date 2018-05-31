b3894d2a6abf83ed75d463c9b9088cac72c991a0
Bart <vdheijden.bart@gmail.com>
2018-05-31 15:29:55 +0200

    update

--- Bouncing.py
+++ Bouncing.py
@@ -41,30 +41,41 @@ def bouncedposition(a,b,p,q):
     n=normalvec(a,b,p,q)
     
     x=[q[0]-s[0],q[1]-s[1]]
-    x=[x[0]*n[0],x[1]*n[1]]
-    x=[x[0]*2,x[1]*2]
+    x=dot(x,n)
+    x=abs(x*2)
+    x=[x*n[0],x*n[1]]
+    print(x)
     
     qr=[x[0]*n[0],x[1]*n[1]]
-    return qr
-
-
-
-
-
     
-a=[1.,4]
-b=[2.,1]
-p=[3.,6]
-q=[1.,2]
+    r=[q[0]+qr[0],q[1]+qr[1]]
+    return r
+
+def bouncedSpeed(v,n):
+    dv=[2*dot(v,n)*n[0],2*dot(v,n)*n[1]]
+    v=[v[0]+dv[0],v[1]+dv[1]]
+    return v
+
+def bounceline(a,b,p,q,v):
+    lam,mu=intersect(a,b,p,q)
+    if lam>=0 and lam<=1 and mu>=0 and mu<=1:
+        bounced = True
+    else:
+        bounced = False
+        
+    r=bouncedposition(a,b,p,q)
+    newv=bouncedSpeed(v,normalvec(a,b,p,q))
     
+    return bounced , r , newv
+        
+p1=[1.,4.]
+p2=[2.,1.]
+prevpos=[3.,6.] 
+pos=[1.,2.]
+v=[(pos[0]-prevpos[0])/0.1,(pos[1]-prevpos[1])/0.1]
 
-s=intersectpoint(a,b,p,q)
-n=normalvec(a,b,p,q)
-qr=bouncedposition(a,b,p,q)
+bounced,newpos,newv=bounceline(p1,p2,prevpos,pos,v)
 
+plt.plot([p1[0],p2[0]])
 
-plt.plot([a[0],b[0]],[a[1],b[1]],label="AB")
-plt.plot([p[0],q[0]],[p[1],q[1]],label="PQ")
-plt.plot([s[0],s[0]+n[0]],[s[1],s[1]+n[1]],label="N")
-plt.plot([q[0],q[0]+qr[0]],[q[1],q[1]+qr[1]],label="QR")
-plt.legend()
+print(bounceline(p1,p2,prevpos,pos,v))   
